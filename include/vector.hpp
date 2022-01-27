eb14b0c8e6488c4accce8080910cbc49cc641c7f
lperson- <lperson-@student.42.fr>
2022-01-27 15:14:51 +0100

    FIX: follow doc for typedefs

--- include/vector.hpp
+++ include/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: lperson- <lperson-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 13:10:41 by lperson-          #+#    #+#             */
-/*   Updated: 2022/01/27 13:28:08 by lperson-         ###   ########.fr       */
+/*   Updated: 2022/01/27 15:13:51 by lperson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,18 @@ namespace ft
 
         typedef T                                           value_type;
         typedef Alloc                                       allocator_type;
-        typedef T &                                         reference;
-        typedef T const &                                   const_reference;
-        typedef T *                                         pointer;
-        typedef T const *                                   const_pointer;
+        typedef typename allocator_type::reference          reference;
+        typedef typename allocator_type::const_reference    const_reference;
+        typedef typename allocator_type::pointer            pointer;
+        typedef typename allocator_type::const_pointer      const_pointer;
         typedef ft::normal_iterator<pointer, Vector>        iterator;
         typedef ft::normal_iterator<const_pointer, Vector>  const_iterator;
         typedef ft::reverse_iterator<iterator>              reverse_iterator;
         typedef ft::reverse_iterator<const_iterator>        
             const_reverse_iterator;
+        typedef typename ft::iterator_traits<iterator>::difference_type
+            difference_type;
         typedef std::size_t                                 size_type;
-        typedef ptrdiff_t                                   difference_type;
     };
 
 };
