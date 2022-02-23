f97be3a9ab7aa3c507ad1eea421b37136278960b
lperson- <lperson-@student.42.fr>
2022-02-23 14:56:00 +0100

    ADD: lexicalographical compare

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -6,7 +6,7 @@
 /*   By: lperson- <lperson-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 12:47:20 by lperson-          #+#    #+#             */
-/*   Updated: 2022/02/23 14:17:44 by lperson-         ###   ########.fr       */
+/*   Updated: 2022/02/23 14:43:56 by lperson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,28 @@ namespace ft
                 return false;
         }
         return true;
+    }
+
+    template <class InputIterator1, class InputIterator2>
+    bool lexicographical_compare(
+        typename enable_if<
+            !is_integral<InputIterator1>::value, InputIterator1
+        >::type first1,
+        InputIterator1 last1,
+        typename enable_if<
+            !is_integral<InputIterator2>::value, InputIterator2
+        >::type first2,
+        InputIterator2 last2
+    )
+    {
+        for (; first1 != last1; ++first1, ++first2)
+        {
+            if (first2 == last2 || *first2 < *first1)
+                return false;
+            else if (*first1 < *first2)
+                return true;
+        }
+        return first2 != last2;
     }
 };
 
