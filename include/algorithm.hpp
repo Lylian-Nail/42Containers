f32f702cf6f387c0ec878ceec4a81ee48ca3e3f5
lperson- <lperson-@student.42.fr>
2022-02-23 13:59:33 +0100

    ADD: equal compare algorithm

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -6,7 +6,7 @@
 /*   By: lperson- <lperson-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 12:47:20 by lperson-          #+#    #+#             */
-/*   Updated: 2022/02/23 13:55:16 by lperson-         ###   ########.fr       */
+/*   Updated: 2022/02/23 13:58:40 by lperson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,28 @@
 
 # define ALGORITHM_HPP
 
+# include "type_traits.hpp"
+
 namespace ft
 {
-
+    template <class InputIterator1, class InputIterator2>
+    bool equal(
+        typename enable_if<
+            !is_integral<InputIterator1>, InputIterator1
+        >::type first1,
+        InputIterator2 last1,
+        typename enable_if<
+            !is_integral<InputIterator2>, InputIterator2
+        >::type first2
+    )
+    {
+        for (; first1 != last1; ++first1, ++first2)
+        {
+            if (*first1 != *first2)
+                return false;
+        }
+        return true;
+    }
 };
 
 #endif
