7f4c276332df82a7bf0f5204ebb73bca2ac195a7
lperson- <lperson-@student.42.fr>
2022-03-15 09:59:53 +0100

    CHANGE: delete implementation

--- include/binary_tree/BinaryTree_implementation.hpp
+++ include/binary_tree/BinaryTree_implementation.hpp
@@ -6,7 +6,7 @@
 /*   By: lperson- <lperson-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 10:21:51 by lperson-          #+#    #+#             */
-/*   Updated: 2022/02/28 16:28:43 by lperson-         ###   ########.fr       */
+/*   Updated: 2022/03/15 09:59:21 by lperson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,6 @@ namespace ft
     /*
      * Constructors and destructor
     */
-
-    // template <class T, class Compare, class Alloc>
-    // BinaryTree<T, Compare, Alloc>::BinaryTree():
-    //     m_root(NULL),
-    //     m_size(0),
-    //     m_compare(),
-    //     m_allocator()
-    // {
-    // }
 
     template <class T, class Compare, class Alloc>
     BinaryTree<T, Compare, Alloc>::BinaryTree(
